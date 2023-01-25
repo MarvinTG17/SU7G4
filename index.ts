e407c32baba47cfe493344d1d05a7df2173f1fa7
Alex Barreto <113317048+alexbarretoz@users.noreply.github.com>
2023-01-24 21:19:36 -0500

    Update index.ts
    agregando crear canciones, listar y login

--- index.ts
+++ index.ts
@@ -52,6 +52,77 @@ app.post("/createPlaylist", async (req: Request,res: Response) => {
 });
 
 
+// Crear canciones
+app.post("/createSong", async (req: Request,res: Response) => {
+    const {name, artist, album, year, genre, duration, nameplaylist } = req.body;
+    const result = await prisma.song.create({
+        data: {
+          name: name,
+          artist:artist,
+          album:album,
+          year:year,
+          genre:genre,
+          duration:duration,
+          playlist: {connect: {name:nameplaylist}},
+        },
+    });
+    res.json(result)        
+});
+
+// listar playlist
+app.get("/playlists", async (req: Request,res: Response) => {
+    const songs = await prisma.playlist.findMany({select:{
+        id:true,
+        name:true,
+        userId: true,
+        songs:true
+    }});
+    return res.json(songs);
+});
+
+
+//listar canciones
+app.get("/songs", async (req: Request,res: Response) => {
+    const songs = await prisma.song.findMany();
+    return res.json(songs);
+});
+
+// buscar cancion por id
+app.post('/songs/:id', async (req:Request, res:Response) => {
+    const {id} = req.params 
+    const songs = await prisma.song.findUnique({
+      where :{
+        id: Number(id)
+      },
+      select:{
+        id:true,
+        name: true,
+        artist: true,
+        album: true
+      }
+    });
+    res.json(songs);
+});
+
+//Crear Login
+app.post("/login",async (req:Request, res:Response) => {
+    const {email,password} = req.body;
+    const user = await prisma.usuarios.findUnique({
+      where :{
+        email:email,
+      }
+    });
+    if (user) {
+      const validatePassword = await bcrypt.compare(password, user?.password as string)
+      //console.log(validatePassword)
+      validatePassword ? res.json("Usuario logueado") : res.json("Contraseña incorrecta")
+    }else{
+      res.json("El email no existe")
+    }
+})
+
+
+
 
 // la funcion flecha es anonima
 app.listen(port, () =>{
